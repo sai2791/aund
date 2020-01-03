1036f3127c464086ec77a300652067bb1b05f771
Steve Inglis <sai2791@googlemail.com>
2020-01-03 20:15:17 +0000

    Update the Version number

--- config.h
+++ config.h
@@ -50,7 +50,7 @@
 #define PACKAGE_NAME "aund"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "aund 1.04"
+#define PACKAGE_STRING "aund 1.05"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "aund"
@@ -59,13 +59,13 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.04"
+#define PACKAGE_VERSION "0.05"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.04"
+#define VERSION "1.05"
 
 /* Define to 1 if `lex' declares `yytext' as a `char *' by default, not a
    `char[]'. */
