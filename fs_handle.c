a5d2ef7f8875ee039ed9e2f23a45ac6b50b4c505
Steve Inglis <sai2791@googlemail.com>
2021-01-15 19:06:17 +0000

    update tags

--- fs_handle.c
+++ fs_handle.c
@@ -103,6 +103,10 @@ fs_open_handle(struct fs_client *client, char *path, int open_flags,
     // permisions for openin, openout, and openup
 
     if (debug) printf("Open handle called ...\n");
+    
+    // We need to get from the fs_client to the fs_context to do 
+    // the ownership testing.  So how do we go about that?
+
 	h = fs_alloc_handle(client, for_open);
 	if (h == 0) {
 		errno = EMFILE;
